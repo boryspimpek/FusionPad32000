377287ee98a79aada1cdbd9d3d17dfe0b1071253
boryspimpek <borysgame@gmail.com>
2026-02-07 16:33:38 +0100

    a

--- menu.py
+++ menu.py
@@ -20,40 +20,59 @@ def show(tft):
     BLACK = ST7735.TFT.BLACK
     WHITE = ST7735.TFT.WHITE
     CYAN  = ST7735.TFT.CYAN
+    GREY  = 0x7BEF # Ciemniejszy szary (zakładając format 565)
 
-    HEADER_Y = 10
-    LINE_Y   = 25
-    MENU_Y   = 40
+    # --- Layout ---
+    MENU_X = 25  # Przesunięcie tekstu w prawo
+    CURSOR_X = 10 
+    START_Y = 45
+    SPACING = 22
 
-    # --- Stałe elementy ---
     tft.fill(BLACK)
-    tft.text((30, HEADER_Y), "SYSTEM V1.0", CYAN, FONT, 1)
-    tft.hline((0, LINE_Y), 160, WHITE)
+    
+    # Nagłówek w ramce
+    tft.rect((5, 5), (150, 25), CYAN)
+    tft.text((35, 13), "CONTROL MODE", CYAN, FONT, 1)
+    
+    # Linia boczna (dekoracyjna)
+    tft.vline((18, START_Y - 5), 70, GREY)
 
-    # Rysuj menu bazowe (bez zaznaczenia)
+    # Statyczne rysowanie opcji
     for i, opcja in enumerate(opcje):
-        y = MENU_Y + i * 20
-        tft.text((20, y), opcja, WHITE, FONT, 1)
+        y = START_Y + i * SPACING
+        tft.text((MENU_X, y), opcja, WHITE, FONT, 1)
 
     while True:
         pots = joystick.get_potentiometers()
         btns = buttons.get_data()
 
-        nowy_wybor = min(int((pots['pot2'] * ilosc) / 101), ilosc - 1)
+        # Lepsze mapowanie (odporne na drgania potencjometru)
+        val = pots['pot2']
+        nowy_wybor = min(int((val * ilosc) / 101), ilosc - 1)
 
         if nowy_wybor != wybrany:
-            # Usuń stary tick
+            # 1. Wyczyść stary kursor I starą pozycję tekstu (opcjonalnie)
             if wybrany != -1:
-                old_y = MENU_Y + wybrany * 20
-                tft.text((8, old_y), " ", WHITE, FONT, 1)
+                old_y = START_Y + wybrany * SPACING
+                tft.text((CURSOR_X, old_y), " ", BLACK, FONT, 1) # Usuń kursor
+                # Przywróć kolor szary dla niewybranych (opcjonalnie)
+                tft.text((MENU_X, old_y), opcje[wybrany], GREY, FONT, 1)
 
-            # Narysuj nowy tick
+            # 2. Narysuj nowy kursor i wyróżnij tekst
             wybrany = nowy_wybor
-            new_y = MENU_Y + wybrany * 20
-            tft.text((8, new_y), ">", WHITE, FONT, 1)
+            new_y = START_Y + wybrany * SPACING
+            
+            tft.text((CURSOR_X, new_y), ">", CYAN, FONT, 1) # Kursor w kolorze CYAN
+            tft.text((MENU_X, new_y), opcje[wybrany], WHITE, FONT, 1) # Tekst na biało
 
-        # Potwierdzenie
         if btns['sw1']:
+            # Efekt "mignięcia" przy wyborze dla feedbacku
+            for _ in range(3):
+                tft.text((CURSOR_X, START_Y + wybrany * SPACING), ">", BLACK, FONT, 1)
+                time.sleep(0.05)
+                tft.text((CURSOR_X, START_Y + wybrany * SPACING), ">", CYAN, FONT, 1)
+                time.sleep(0.05)
+            
             while buttons.get_data()['sw1']:
                 time.sleep(0.01)
             return opcje[wybrany]
